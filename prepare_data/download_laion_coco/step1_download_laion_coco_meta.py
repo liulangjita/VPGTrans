676fcf5595f6a8bb8baab1e89eb8c688c2d4bcc9
liulangjita <jiabin-101@163.com>
2023-05-23 14:27:04 +0800

    Update step1_download_laion_coco_meta.py

--- prepare_data/download_laion_coco/step1_download_laion_coco_meta.py
+++ prepare_data/download_laion_coco/step1_download_laion_coco_meta.py
@@ -2,6 +2,11 @@ from huggingface_hub import hf_hub_download
 import os
 
 for i in range(0, 15):
-    hf_hub_download(local_dir_use_symlinks=False, repo_id="laion/laion-coco", \
+    if i < 10:
+        hf_hub_download(local_dir_use_symlinks=False, repo_id="laion/laion-coco", \
                     filename=f"part-0000{i}-2256f782-126f-4dc6-b9c6-e6757637749d-c000.snappy.parquet", \
+                    repo_type="dataset", local_dir="./meta")
+    else:
+        hf_hub_download(local_dir_use_symlinks=False, repo_id="laion/laion-coco", \
+                    filename=f"part-000{i}-2256f782-126f-4dc6-b9c6-e6757637749d-c000.snappy.parquet", \
                     repo_type="dataset", local_dir="./meta")
